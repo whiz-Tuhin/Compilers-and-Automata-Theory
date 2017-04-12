f99949a522c0b7bab73196b4b5e1ae224a3548aa
Tuhin Khare <tuhinkhare3@gmail.com>
2017-04-12 10:52:49 +0530

    Code Still buggy

---
+++ Recursive Descent Parsing/lexp_grmr.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+
+using namespace std;
+#define BUFFER_SIZE 256
+
+void getNextToken();
+void list();
+void aterm();
+void lexpseq();
+void lexpseq1();
+void lexp();
+
+char* input;
+char* token_pointer;
+char token[BUFFER_SIZE];
+
+FILE* fp;
+
+void current_token(){
+    cout<<"Currently at token  => "<<token<<endl;
+}
+
+void getNextToken(){
+      int k = 0;
+      char c = fgetc(fp);
+      if(c == '$'){
+        token[k++] = c;
+        token[k] = '\0';
+      }
+      else{
+      while(c != ' '){
+          token[k++] = c;
+          c = fgetc(fp);
+      }
+      token[k] = '\0';
+     }
+
+      cout<<"Token = "<<token<<endl;
+}
+
+void lexp(){
+    cout<<"Inside lexp"<<endl;
+    current_token();
+    if(strcmp(token,"(") == 0){
+          list();
+          getNextToken();
+    } else if(strcmp(token,"number") == 0 || strcmp(token,"identifier") == 0){
+          aterm();
+    }else{
+      cout<<"Error"<<endl;
+    }
+}
+
+void list(){
+    cout<<"Inside list"<<endl;
+    if(strcmp(token,"(") == 0){
+          current_token();
+          getNextToken();
+          lexpseq();
+          getNextToken();
+          if(strcmp(token,")") == 0){
+                cout<<"final stuffs"<<endl;
+                getNextToken();
+                current_token();
+                if(strcmp(token,"$") == 0){
+                        cout<<"Success"<<endl;
+                        exit(0);
+                }
+          } else {
+                  cout<<"Error"<<endl;
+          }
+    } else {
+          cout<<"Error"<<endl;
+    }
+}
+
+void aterm(){
+      cout<<"Inside aterm"<<endl;
+      if(strcmp(token,"number") == 0){
+              current_token();
+      } else if(strcmp(token,"identifier") == 0){
+              current_token();
+      } else{
+          cout<<"Error"<<endl;
+      }
+}
+
+void lexpseq(){
+      cout<<"Inside lexpseq"<<endl;
+      lexp();
+      getNextToken();
+      lexpseq1();
+}
+
+void lexpseq1(){
+      cout<<"Inside lexpseq1"<<endl;
+      if(strcmp(token,"(") == 0 || strcmp(token,"number") == 0 || strcmp(token,"identifier") == 0){
+            lexp();
+            lexpseq1();
+      }
+}
+
+
+int main(){
+
+        fp = fopen("input.txt","r");
+        getNextToken();
+        lexp();
+        return 0;
+}
